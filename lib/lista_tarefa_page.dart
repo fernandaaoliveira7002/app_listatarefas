2715b69a3cec5bff1dc22a4672e0483a6e995d63
fernanda alves de oliveira <fernandaaoliveira7002@gmail.com>
2026-09-01 14:11:11 -0300

    fase b

--- lib/lista_tarefa_page.dart
+++ lib/lista_tarefa_page.dart
@@ -11,6 +11,7 @@ class ListaTarefaPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: ListView(
+        padding: EdgeInsets.all(12),
         children: [
           Card(
             child: ListTile(
@@ -18,11 +19,58 @@ class ListaTarefaPage extends StatelessWidget {
                 Icons.check_circle,
                 color: Colors.green,
               ),
-              title: Text("Configurar o ambiente de desenvolvimento"),
+              title: Text(
+                "Configurar o ambiente de desenvolvimento",
+                style: TextStyle(decoration: TextDecoration.lineThrough),
+              ),
               subtitle: Text("Concluída"),
+              trailing: Icon(
+                Icons.delete_outline,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.circle_outlined,
+                color: Colors.blueGrey,
+              ),
+              title: Text(
+                "Fazer a atividade Flutter",
+              ),
+              subtitle: Text("Pendente"),
+              trailing: Icon(
+                Icons.delete_outline,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.check_circle,
+                color: Colors.green,
+              ),
+              title: Text(
+                "Arrumar um emprego",
+                style: TextStyle(decoration: TextDecoration.lineThrough),
+              ),
+              subtitle: Text("Concluída"),
+              trailing: Icon(
+                Icons.delete_outline,
+                color: Colors.grey,
+              ),
             ),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+        //shape:CircleBorder(),
+        child: Icon(Icons.add),
       ),
     );
   }
